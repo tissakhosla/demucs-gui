5f0b5db181041baa00af7773912fd66131b11839
tissa-ctmb <tissa.music@gmail.com>
2023-01-03 21:57:47 -0500

    queue v1 - stops for some reason, but not fatal

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ import smtplib
 import uuid
 import logging
 import sqlite3
+
 from datetime import datetime
 from threading import Thread
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
 
 from flask import (
     Flask, render_template, request,
@@ -23,11 +28,16 @@ logging.basicConfig(level=logging.INFO)
 
 # initialize db
 TBL = 'track'
+
 con = sqlite3.connect("tracks.db", check_same_thread=False)
 cur = con.cursor()
+
 trackTable = f'CREATE TABLE IF NOT EXISTS  \
-            {TBL}(title, timestamp, user, code)'
+            {TBL}(title, timestamp, user, code, status)'
+
 cur.execute(trackTable)
+
+# initialize Flask App
 
 UPLOAD_FOLDER = '/tmp'
 ALLOWED_EXTENSIONS = { 'wav' }
@@ -36,33 +46,72 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = os.urandom(12)
 
+# confirm env vars
+
 assert os.getenv('G_SMTP')
 assert os.getenv('G_MAIL')
 assert os.getenv('G_KEY')
 
-def insert(fn, ts, ue, cn):
+# TODO: DB functions - Make into util class
+# create
+def db_insert(fn, ts, ue, cn, status):
     '''insert track data into db'''
     sqlcmd = f"INSERT into {TBL} VALUES \
-    ('{fn}', {ts}, '{ue}', '{cn}')"
+    ('{fn}', {ts}, '{ue}', '{cn}', '{status}')"
     logging.info(' < SQLCMD: %s', sqlcmd)
     cur.execute(sqlcmd)
     con.commit()
 
-def utime():
-    '''create timestamp when called'''
-    dt = datetime.now()
-    print(dt)
-    return datetime.timestamp(dt)
+# read
+def db_all():
+    sqlcmd = "SELECT * FROM track"
+    res = cur.execute(sqlcmd)
+    return res.fetchall()
 
-def demucs(wav, f, em, url):
-    os.system(f'demucs /tmp/{wav}')
-    email(wav, f, em, url)
+def db_toptrack():
+    sqlcmd = "SELECT * FROM track ORDER BY timestamp"
+    res = cur.execute(sqlcmd)
+    return res.fetchone()
+
+# update
+def db_update(uid):
+    '''update demucsing track status to D'''
+    sqlcmd = f"UPDATE track SET status = 'D' WHERE code = '{uid}'"
+    res = cur.execute(sqlcmd)
+    logging.info(' UPDATE: %s', res)
+    con.commit()
+
+# delete
+def db_delete(uid):
+    '''delete track'''
+    sqlcmd = f"DELETE from track WHERE code = '{uid}'"
+    res = cur.execute(sqlcmd)
+    logging.info(' DELETE: %s', res)
+    con.commit()
+
+# TODO: App functions - Make into util class
 
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
-def email(c, f, e, u):
+def check_status(s):
+    for t in db_all():
+        if t[4] == s:
+            return True
+
+def utime():
+    '''create timestamp when called'''
+    dt = datetime.now()
+    return datetime.timestamp(dt)
+
+def demucs(wav):
+    '''run demucs and send email'''
+    os.system(f'demucs /tmp/{wav}')
+    db_delete(wav)
+    pp.pprint(db_all())
+
+def email(uid, fn, em, ip):
     '''send email'''
     # Set up the SMTP server
     server = smtplib.SMTP(os.getenv('G_SMTP'), 587)
@@ -72,16 +121,16 @@ def email(c, f, e, u):
     me = os.getenv('G_MAIL')
     server.login(me, os.getenv('G_KEY'))
 
-    logging.info(' > to: %s', e)
+    logging.info(' > to: %s', em)
     # Build the email
-    to = [e]
-    subject = f'{f} Split Tracks'
+    to = [em]
+    subject = f'{fn} Split Tracks'
 
     body = f'Here are the links your files: \
-        \n\n http://{u}/separated/htdemucs/{c}/bass.wav \
-        \n http://{u}/separated/htdemucs/{c}/drums.wav \
-        \n http://{u}/separated/htdemucs/{c}/other.wav \
-        \n http://{u}/separated/htdemucs/{c}/vocals.wav'
+        \n\n http://{ip}/separated/htdemucs/{uid}/bass.wav \
+        \n http://{ip}/separated/htdemucs/{uid}/drums.wav \
+        \n http://{ip}/separated/htdemucs/{uid}/other.wav \
+        \n http://{ip}/separated/htdemucs/{uid}/vocals.wav'
 
     msg = f'Subject: {subject}\n\n{body}'
     server.sendmail(me, to, msg)
@@ -89,9 +138,25 @@ def email(c, f, e, u):
     # Disconnect from the server
     server.quit()
 
+def process(host):
+    '''process the oldest track in the db'''
+    t = db_toptrack()
+    db_update(t[3])
+    demucs(t[3])
+    email(t[3], t[0], t[2], host)
+
+def process_tracks(req_host):
+    '''process all tracks in DB'''
+    process(req_host)
+    if check_status('W'):
+        process(req_host)
+    if check_status('N'):
+        process(req_host)
+    else:
+        print("no tracks left")
+
 @app.route("/")
 def to_upload():
-    print(utime())
     return redirect('/upload')
 
 @app.route("/upload", methods=['GET', 'POST'])
@@ -99,6 +164,8 @@ def upload_file():
     '''main route'''
     logging.info(' START: a wild user has appeared')
     logging.info(' SERVER: %s', request.host)
+    logging.info(' CLIENT: %s', request.environ['REMOTE_ADDR'])
+
     if request.method == 'POST':
 
         if 'file' not in request.files:
@@ -113,26 +180,35 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
 
-            logging.info(' < file: %s', filename)
-            logging.info(' < type: %s', file.content_type)
-            logging.info(' < size: %s bytes', len(file.read()))
+            logging.info(' < FILE: %s', filename)
+            logging.info(' < TYPE: %s', file.content_type)
+            logging.info(' < SIZE: %s bytes', len(file.read()))
             file.seek(0)
             codename = str(uuid.uuid4())
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], codename))
 
-            insert(filename, utime(), useremail, codename)
+            # write file to FS
+            fp = os.path.join(app.config['UPLOAD_FOLDER'], codename)
+            file.save(fp)
+            # TODO: assert os.path.exists(fp)
 
             return redirect(url_for('success', cn=codename, fn=filename, ue=useremail))
     return render_template('upload.html')
 
 @app.route('/success-<cn>')
 def success(cn):
-    '''show success after processing'''
+    '''show success after saving file'''
     os.system(f'ls /tmp/{cn}')
     ue = request.args.get('ue')
     fn = request.args.get('fn')
-    _demucs = Thread(target=demucs, args=(cn, fn, ue, request.host))
-    # _demucs.start()
+
+    if check_status('D'):
+        db_insert(fn, utime(), ue, cn, 'W')
+    else:
+        db_insert(fn, utime(), ue, cn, 'N')
+
+    _process_tracks = Thread(target=process_tracks, args=(request.host,))
+    _process_tracks.start()
+
     return render_template('success.html', ue=ue, fn=fn)
 
 @app.route('/separated/<path:filename>')
@@ -140,7 +216,7 @@ def serve_static(filename):
     return send_from_directory('separated', filename)
 
 
-# TODO: use sqlite3
+
 # TODO: get better variable names throughout
 # TODO: add start time and end time to job
 # TODO: use flask-session to keep state
